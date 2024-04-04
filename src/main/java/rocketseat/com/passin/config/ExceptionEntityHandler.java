7a0b521604fa4d3937da8ce7f6466e04e20cc3b4
Jefferson Guanabara <jefferson.guanabara@dcx.ufpb.br>
2024-04-04 16:24:24 -0300

    feat: exception handling

--- src/main/java/rocketseat/com/passin/config/ExceptionEntityHandler.java
+++ src/main/java/rocketseat/com/passin/config/ExceptionEntityHandler.java
@@ -1,9 +1,15 @@
 package rocketseat.com.passin.config;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import rocketseat.com.passin.domain.attendee.exceptions.AttendeeAlreadyExistException;
+import rocketseat.com.passin.domain.attendee.exceptions.AttendeeNotFoundException;
+import rocketseat.com.passin.domain.checkin.exceptions.CheckInAlreadyExistsException;
+import rocketseat.com.passin.domain.event.exceptions.EventFullException;
 import rocketseat.com.passin.domain.event.exceptions.EventNotFoundException;
+import rocketseat.com.passin.dto.general.ErrorResponseDTO;
 
 @ControllerAdvice
 public class ExceptionEntityHandler {
@@ -12,4 +18,27 @@ public class ExceptionEntityHandler {
     public ResponseEntity handleEventNotFound(){
         return ResponseEntity.notFound().build();
     }
+
+    @ExceptionHandler(EventFullException.class)
+    public ResponseEntity handleEventFull(EventFullException eventFullException){
+        return ResponseEntity.badRequest()
+                .body(new ErrorResponseDTO(eventFullException.getMessage()));
+    }
+
+    @ExceptionHandler(AttendeeNotFoundException.class)
+    public ResponseEntity handleAttendeeNotFound(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(AttendeeAlreadyExistException.class)
+    public ResponseEntity handleAttendeeAlreadyExists(){
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+
+    @ExceptionHandler(CheckInAlreadyExistsException.class)
+    public ResponseEntity handleCheckInAlreadyExists(){ return ResponseEntity.status(HttpStatus.CONFLICT).build(); }
+
+
+
+
 }
---
+++ src/main/java/rocketseat/com/passin/dto/general/ErrorResponseDTO.java
@@ -0,0 +1,6 @@
+package rocketseat.com.passin.dto.general;
+
+public record ErrorResponseDTO(
+        String message
+) {
+}
