bcc66fad7d580d858eb772bbf3d45d73ec193a06
Jefferson Guanabara <jefferson.guanabara@dcx.ufpb.br>
2024-04-04 01:45:17 -0300

    feat: implementing participant search service by event ID

--- src/main/java/rocketseat/com/passin/repositories/CheckinRepository.java
+++ src/main/java/rocketseat/com/passin/repositories/CheckinRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import rocketseat.com.passin.domain.checkin.Checkin;
 
+import java.util.Optional;
+
 @Repository
 public interface CheckinRepository extends JpaRepository<Checkin, Integer> {
+    Optional<Checkin> findByAttendeeId(String attendeeId);
 }
--- src/main/java/rocketseat/com/passin/services/EventService.java
+++ src/main/java/rocketseat/com/passin/services/EventService.java
@@ -19,11 +19,11 @@ import java.util.List;
 public class EventService {
 
     private final EventRepository eventRepository;
-    private final AttendeeRepository attendeeRepository;
+    private final AttendeeService attendeeService;
     public EventResponseDTO getEventDetails(String eventId) {
         Event event = this.eventRepository.findById(eventId)
                 .orElseThrow(() -> new EventNotFoundException("Event not found with ID: " + eventId));
-        List<Attendee> attendeeList = this.attendeeRepository.findByEventId(eventId);
+        List<Attendee> attendeeList = this.attendeeService.getAttendeesFromEvents(eventId);
         return new EventResponseDTO(event, attendeeList.size());
     }
 
